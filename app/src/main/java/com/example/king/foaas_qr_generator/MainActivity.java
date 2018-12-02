e4cd7be2859167690e71e8e704ded245bdc96f98
coreyshen10 <31442068+coreyshen10@users.noreply.github.com>
2018-12-02 16:09:47 -0600

    wtf am i doing... HOW TO MAKE VOLLEY WORK. BUILD DOES NOT WORKKK

--- app/src/main/java/com/example/king/foaas_qr_generator/MainActivity.java
+++ app/src/main/java/com/example/king/foaas_qr_generator/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.king.foaas_qr_generator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+/**
+ * main Screen for app.
+ */
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "FOAAS_QR_Generator:Main";
+    private static RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        // Load the main layout for our activity
+        setContentView(R.layout.activity_main);
+
+        // Attach the handler to our UI button
+        final Button startAPICall = findViewById(R.id.startAPICall);
+        startAPICall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Start API button clicked");
+                startAPICall();
+            }
+        });
+    }
+
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Fuck%20Off\n",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                            ImageView qrCode = findViewById(R.id.qrCode);
+                            qrCode.setImageResource(R.drawable.);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
